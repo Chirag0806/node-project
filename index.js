b406b021c8d39200caa50dba619d14a28a13881e
Chiragagile <chirag.c@agile-labs.com>
2024-12-17 15:45:26 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+
+const colors = require('colors')
+
+console.log("hello".blue);
+
+// const app = require('./app')
+
+// console.log("This is my code");
+// var a = 10;
+// let b = 20;
+// const c = 30;
+// console.log(a+b+c);
+
+// if(a == 20){
+//     console.log('matched');
+// }
+
+ // console.log(app.z());
