612b1ef69c2ad6499de5b51b4b60a815ce5b048a
Chiragagile <chiragchandak.it2016@gmail.com>
2025-01-26 20:09:28 +0530

    changes

---
+++ data.js
@@ -0,0 +1,3 @@
+const data = {name:'Chirag Chandak',email:'test@gmail.com'}
+const data1 = [{name:'Chirag Chandak',email:'test@gmail.com'},{name:'Chirag Chandak',email:'test@gmail.com'},{name:'Chirag Chandak',email:'test@gmail.com'}]
+module.exports =data,data1;
--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 
 const colors = require('colors')
+const http = require('http');
+const data = require('./data');
+const data1 = require('./data');
+const fs = require('fs');
+const path = require('path');
+const { isUtf8 } = require('buffer');
 
-console.log("hello".blue);
+// http.createServer((req,res)=>{
+// res.writeHead(200,{'content-type':'application\json'});
+// res.write(JSON.stringify(data));
+// res.end();
+// }).listen(5000);
+
+// console.log("hello".blue);
 
 // const app = require('./app')
 
@@ -16,3 +28,66 @@ console.log("hello".blue);
 // }
 
  // console.log(app.z());
+//  console.warn("eiof")
+
+
+//  FileSystem  
+
+//  const input = process.argv;
+
+//  if(input[2] == "add"){
+//     fs.writeFileSync(input[3],input[4]);
+//  }else if(input[2] == 'remove'){
+//     fs.unlinkSync(input[3])
+//  }else{
+//     console.log("invalid input")
+//  }
+
+
+
+// const dirPath = path.join(__dirname,'files');
+// console.warn(dirPath);
+// for(i=0;i<5;i++){
+//     fs.writeFileSync(dirPath + "/hello" + i + ".txt" ,"this is my text file")
+// }
+
+// fs.readdir(dirPath,(error,files) => {
+//    files.forEach((file)=>{
+//      console.log("File name is " + file)
+//    })
+// });
+ // const filePath = `${dirPath}/hello1.txt`;
+// fs.readFile(filePath,'utf-8',(error,item)=>{
+//     console.log(item)
+// })
+
+// fs.appendFile(filePath," and i am addind some new data",(error)=>{
+//  if(!error) console.log("file is updated");
+// })
+
+// fs.rename(filePath,`${dirPath}/New.txt`,(err)=>{
+//   if(!err) console.log("File name is updated")
+// })
+
+// fs.unlinkSync(`${dirPath}/New.txt`);
+
+let a = 20;
+let b = 0;
+
+let waitingData = new Promise((resolve,reject)=>{
+   setTimeout(()=>{
+     resolve(30)
+   },2000)
+})
+
+waitingData.then((data)=>{
+ console.log(a+data)
+})
+
+
+
+
+
+
+ 
+
